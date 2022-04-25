fd776acf0cba20c8a1de2678753d7cd3c2b45490
AidenMei1102 <59463077+aidenmei@users.noreply.github.com>
2022-04-24 23:28:45 -0400

    Search for products by category

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
