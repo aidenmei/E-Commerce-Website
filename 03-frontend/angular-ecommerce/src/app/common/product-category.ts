367277b3fe48c5f4d28ffaf338623320c7b0a1e2
AidenMei1102 <59463077+aidenmei@users.noreply.github.com>
2022-04-29 17:58:53 -0400

    search for product by Category(Dynamic search component

---
+++ 03-frontend/angular-ecommerce/src/app/common/product-category.ts
@@ -0,0 +1,7 @@
+export class ProductCategory {
+    getProductCategories() {
+      throw new Error('Method not implemented.');
+    }
+    id : number | undefined;
+    categoryName: string | undefined;
+}
