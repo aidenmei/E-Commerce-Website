52bf154444c4489391d82bca30809980477f70d8
AidenMei1102 <59463077+aidenmei@users.noreply.github.com>
2022-05-01 15:54:38 -0400

    search for products by keyword

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
+    // behind the scenes: SELECT * FROM Product p WHERE p.name LIKE CONCAT('%', :name, '%')
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -12,37 +12,61 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] | undefined;
   currentCategoryId: number | undefined;
+  searchMode: boolean | undefined;
 
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
-  //inject our ProductService
-  constructor(private productListService: ProductService,
-              private route: ActivatedRoute) { } //inject the ActivatedRoute
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
-    this.listProducts();
+      this.listProducts();
     });
   }
 
   listProducts() {
-    // check if "id" parameter is available
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if (hasCategoryId) {
-      // get the "id" param string. convert string to a number using the "+" symbol
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!; // class #78
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
     }
     else {
-      // not category id available ... default to category id 1
-      this.currentCategoryId = 1;
+      this.handleListProducts();
     }
 
-    // now get the product for the given category id 
-    this.productListService.getProductList(this.currentCategoryId).subscribe(
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
       data => {
         this.products = data;
       }
     )
   }
 
+  handleListProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )    
+  }
 }
